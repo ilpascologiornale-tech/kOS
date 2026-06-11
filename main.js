edea3d17ed0d2e83cc79fb2719bb1a76a6da8c5a
ilpascologiornale-tech <ilpascolo.giornale@gmail.com>
2026-06-11 15:26:07 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow, autoUpdater } = require('electron');
+const path = require('path');
+if (require('electron-squirrel-startup')) app.quit();
+autoUpdater.setFeedURL('https://github.com/Ilpascologiornale-tech/kOS-XP/releases/latest');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024, height: 768,
+    icon: path.join(__dirname, 'icon.ico'),
+    title: 'kOS XP Professional',
+    autoHideMenuBar: true,
+    webPreferences: { nodeIntegration: false, contextIsolation: true, webSecurity: false }
+  });
+  win.loadFile('kOS.html');
+  win.webContents.on('did-finish-load', () => autoUpdater.checkForUpdates());
+  setInterval(() => autoUpdater.checkForUpdates(), 1800000);
+}
+app.whenReady().then(createWindow);
+autoUpdater.on('update-downloaded', () => autoUpdater.quitAndInstall());
+app.on('window-all-closed', () => app.quit());
